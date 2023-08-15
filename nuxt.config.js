bc6e343761a3cb0d2cd62158eae95a7f864dc467
Andri Nugroho <Gachotech1@>
2023-08-15 13:32:52 +0700

    internal server error

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,12 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  ssr: false,
   head: {
     title: 'Adiba Store',
     htmlAttrs: {
       lang: 'en'
     },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
